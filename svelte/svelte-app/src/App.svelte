2c556a73c08038a2b561fdf3d7bf0eb8b6a67386
Michael Elliott <m.elliott92@yahoo.com>
2023-06-05 21:48:47 -0600

    feat: Add image element

--- svelte/svelte-app/src/App.svelte
+++ svelte/svelte-app/src/App.svelte
@@ -3,6 +3,7 @@
 
     let greeting = '';
     let name = '';
+    let imgSrc = '';
 
     const getGreeting = async (name) => {
         const resp = await fetch('http://localhost:8080/hello?name=' + name);
@@ -10,12 +11,21 @@
         console.log(resp);
         console.log(greeting);
     }
+
+    const getPhoto = async () => {
+      const resp = await fetch('http://localhost:8080/hello');
+      const blob = await resp.blob();
+      imgSrc = URL.createObjectURL(blob);
+    }
 </script>
 
 <h1>Svelte + Spring!</h1>
 <input type="text" bind:value={name}/>
 <button on:click={getGreeting(name)}>Get Greeting</button>
 <p>{greeting}</p>
+{#if imgSrc != ''}
+<img src={imgSrc} alt="Fetched from API" />
+{/if}
 
 <style>
     h1, p {
